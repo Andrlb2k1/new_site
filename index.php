c0c5f3b075592491494d2283ad1612c5c1d06e66
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2024-08-21 14:20:18 -0300

    new archives

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
-<?php  include_once './config/config.php'; ?>
-<?php 
-  if(!isset($_SESSION['userLogged'])){
-    header("Location: login.php");
-  }
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
-  
-  <?php include_once './partials/head.php'; ?>
-
-  <body>    
-
-    <section>
-      <?php include_once './partials/menu.php'; ?>
-
-      <?php include_once './pages/routes.php'; ?>
-    </section>
-    
-    <?php include_once './partials/scripts.php'; ?>
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Em breve</title>
+</head>
+<body>
+    <h1>
+        Em breve estará online.     
+    </h1>
+    <a href="./admin">Admin</a>
+</body>
 </html>
