0666991e4d2e7ba93ea2b0586b1aaa36929c456d
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2024-08-23 13:44:39 -0300

    new php

--- conexao.php
+++ conexao.php
@@ -10,15 +10,46 @@ if ($db -> connect_errno){
 	exit();
 }
 
+//Inserindo dados
+$password = '12112001';
+$insert = "INSERT INTO `users` (
+  `name`,
+  `email`,
+  `password`
+)
+VALUES
+  (
+    'Inserindo via PHP',
+    'andre.lb865850@gmail.com',
+    '{$password}'
+  );";
+
+$result = $db->query($insert);
+
+if($result){
+	echo "Salvo com sucesso ID => {$db->insert_id}";
+}else{
+	echo "Erro no Insert: {$db->error}";
+}
+
+//Consultando dados
 $select = "SELECT * FROM users";
 $result = $db->query($select);
 
 if($result){
-	while($obj = $result->fetch_object()){
-		echo "ID: {$obj->id} <br>";
-		echo "Nome: {$obj->name} <br>";
-		echo "Email: {$obj->email} <br>";
-		echo "Registrado em: {$obj->created_at} <br>";
+	//while($obj = $result->fetch_object()){
+	//	echo "ID: {$obj->id} <br>";
+	//	echo "Nome: {$obj->name} <br>";
+	//	echo "Email: {$obj->email} <br>";
+	//	echo "Registrado em: {$obj->created_at} <br>";
+	//	echo "<hr>";
+	//}
+
+	while($obj = $result->fetch_array()){
+		echo "ID: {$obj['id']} <br>";
+		echo "Nome: {$obj['name']} <br>";
+		echo "Email: {$obj['email']} <br>";
+		echo "Registrado em: {$obj ['created_at']} <br>";
 		echo "<hr>";
 	}
 }else{
